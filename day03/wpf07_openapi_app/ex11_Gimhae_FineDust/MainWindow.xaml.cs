994d39ffe7d720ae0092f3ce1b8ec62d186c4427
kimdongju1 <dkdlkdh@naver.com>
2024-05-09 13:11:10 +0900

    .

---
+++ day03/wpf07_openapi_app/ex11_Gimhae_FineDust/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace ex11_Gimhae_FineDust
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // 실시간조회 버튼 클릭
+        private async void BtnReqRealtime_Click(object sender, RoutedEventArgs e)
+        {
+            string openApiUri = "https://smartcity.gimhae.go.kr/smart_tour/dashboard/api/publicData/dustSensor";
+            string result = string.Empty;
+
+            // WebRequest, WebResponse 객체
+            WebRequest req = null;
+            WebResponse res = null;
+            StreamReader reader = null;
+
+            try
+            {
+                req = WebRequest.Create(openApiUri);
+                res = await req.GetResponseAsync();
+                reader = new StreamReader(res.GetResponseStream());
+                result = await reader.ReadToEndAsync();
+
+                await this.ShowMessageAsync("결과", result);
+            }
+            catch (Exception ex)
+            {
+                await this.ShowMessageAsync("오류", $"OpenApi 조회오류 {ex.Message}");
+            }
+        }
+
+        private void CboReqDate_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnSaveData_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GrdResult_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
